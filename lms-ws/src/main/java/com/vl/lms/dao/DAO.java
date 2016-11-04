e0a73db69fced01bf4a3d0040965ec59204cbd85
lmsvaluelabs <lms.valuelabs@gmail.com>
2016-11-04 18:08:48 +0530

    Save or update method added

--- lms-ws/src/main/java/com/vl/lms/dao/DAO.java
+++ lms-ws/src/main/java/com/vl/lms/dao/DAO.java
@@ -3,5 +3,7 @@ package com.vl.lms.dao;
 import java.io.Serializable;
 
 public interface DAO {
-	<T extends Serializable> void save(T obj);		
+	<T extends Serializable> void save(T obj);
+
+	<T extends Serializable> void saveOrUpdate(T obj);
 }
