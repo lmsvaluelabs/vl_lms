1ed1f8d7d2d865fd00bfaa79e2d93d7d068df54b
lmsvaluelabs <lms.valuelabs@gmail.com>
2016-11-08 11:19:04 +0530

    updated in pom and DAO @Hari

--- lms-ws/src/main/java/com/vl/lms/dao/DAOImpl.java
+++ lms-ws/src/main/java/com/vl/lms/dao/DAOImpl.java
@@ -1,5 +1,19 @@
 package com.vl.lms.dao;
 
+import java.io.Serializable;
+
 public class DAOImpl implements DAO {
+
+	@Override
+	public <T extends Serializable> void save(T obj) {
+	
+		
+	}
+
+	@Override
+	public <T extends Serializable> void saveOrUpdate(T obj) {
+		
+		
+	}
 
 }
