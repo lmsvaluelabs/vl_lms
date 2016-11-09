0474355831586f2607822b9f83d56a436cc133fd
hpolnati <hari.polnati@valuelabs.com>
2016-11-09 17:44:30 +0530

    Dev commit

--- lms-ws/src/main/java/com/vl/lms/builder/LeaveBuilder.java
+++ lms-ws/src/main/java/com/vl/lms/builder/LeaveBuilder.java
@@ -12,7 +12,7 @@ public class LeaveBuilder {
 		if (Objects.nonNull(leaveDTO)) {
 			leave = new Leave();
 			leave.setEmployeeComments(leaveDTO.getEmployeeComments());
-			//leave test 
+			//leave dev commit test 
 		} 
 		return leave;   
 
