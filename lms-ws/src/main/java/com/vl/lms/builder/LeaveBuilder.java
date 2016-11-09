5b621397404114ef5c12e84220405e30f6632b5c
hpolnati <hari.polnati@valuelabs.com>
2016-11-09 16:49:28 +0530

    Master push

--- lms-ws/src/main/java/com/vl/lms/builder/LeaveBuilder.java
+++ lms-ws/src/main/java/com/vl/lms/builder/LeaveBuilder.java
@@ -12,7 +12,7 @@ public class LeaveBuilder {
 		if (Objects.nonNull(leaveDTO)) {
 			leave = new Leave();
 			leave.setEmployeeComments(leaveDTO.getEmployeeComments());
-			//leave test 
+			//leave test MASTER PUSH
 		} 
 		return leave;   
 
