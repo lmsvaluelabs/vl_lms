79c7e8cf20b3b0430925264e092ad774ca2b0d78
karimuddin.syed <karimuddin.syed@valuelabs.com>
2016-11-10 12:08:13 +0530

    Added Leave Resource @Karim

---
+++ lms-ws/src/main/java/com/vl/lms/resource/LeaveResource.java
@@ -0,0 +1,17 @@
+package com.vl.lms.resource;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * @author karimuddin.syed
+ *
+ */
+@Path("employee")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class LeaveResource {
+
+}
